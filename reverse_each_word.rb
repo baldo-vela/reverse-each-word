1c538e8b5c0729c8da69579c2ae861c6e0e8f238
baldo-vela <baldo.vela+github@gmail.com>
2020-11-15 03:00:40 -0600

    commit tree 1d30525b0848ba1c9f40277e48a65315d47a2625

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+# Understand the return value of the each method
+# Use the .collect method
+# Understand the return value of the .collect method
+# Use the return of collect for further operation
+# Instructions
+# Write a method called reverse_each_word that takes in a string argument of a sentence and returns that same sentence with each word reversed in place.
+
+# First solve it using .each Then utilize the same method using .collect to see the difference.
+
+# For example:
+
+# reverse_each_word("Hello there, and how are you?")
+#   #=> "olleH ,ereht dna woh era ?uoy" 
+# Hint: You can't use an enumerator on a string, so how can we turn our string into an array?
+
+# Hint: How can we reverse each word and return those altered words? Remember that .each returns the original array but other enumerators don't.
+
+def reverse_each_word(sentence)
+    sentence.split.collect(&:reverse).join " "
+end
